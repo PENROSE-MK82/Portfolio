10d799055811426b6fb66b9b34c7228e9dffdc09
cookapps-yhjeong <32216564+PENROSE-MK82@users.noreply.github.com>
2022-06-13 23:18:58 +0900

    두트윈 추가

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private Material scanningMat;
+    [SerializeField] private Material shieldMat;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private void Initialize()
+    {
+        
+    }
+    
+    private void ShowCity()
+    {
+        
+    }
+
+    private void ShowShield()
+    {
+        
+    }
+
+    private void StartShootMissile()
+    {
+        
+    }
+}
